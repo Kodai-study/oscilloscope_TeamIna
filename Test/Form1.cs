20ae6f90f933ddcc2b24dfe3d6dbfabac9766456
Kodai-study <anchor.kou@softbank.ne.jp>
2023-07-20 11:24:17 +0900

    テストデータをプロット
    ボタンを押すごとにテストデータを受信し、それをそのままプロット

--- Test/Form1.cs
+++ Test/Form1.cs
@@ -7,15 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ScottPlot.Renderable;
 
 namespace Test
 {
     public partial class Form1 : Form
     {
+        Plotter plotter;
+        DataReceiver dataReceiver;
         public Form1()
         {
             InitializeComponent();
             this.button1.Click += (sender, e) => { this.label1.Text = "Clicked"; };
+            
+
+            this.plotter = new(formsPlot1);
+            dataReceiver = new();
+           
         }
         
         private void button1_Click(object sender, EventArgs e)
@@ -30,7 +38,17 @@ namespace Test
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            long u_sec;
+            double voltage;
+            if(dataReceiver.GetVoltageData(out u_sec, out voltage))
+            {
+                this.plotter.registerData(u_sec, voltage);
+            }
+            formsPlot1.Plot.SetAxisLimitsX(xMin: 0, xMax: 0.01);
+            //formsPlot1.Plot.AxisAutoX();
 
+            formsPlot1.Plot.AxisAutoY();
+            this.formsPlot1.Refresh();
         }
     }
 }
--- Test/Plotter.cs
+++ Test/Plotter.cs
@@ -10,11 +10,20 @@ namespace Test
     public class Plotter
     {
         private FormsPlot formPlot;
+
+        private List<double> voltageDataList;
+        private List<double> timeDataList;
         public Plotter(FormsPlot formPlot)
         {
             this.formPlot = formPlot;
+            voltageDataList = new List<double>();
+            timeDataList = new List<double>();
         }
-        private double[] data;
-        public void registerData(long u_sec, double data) { }
+        public void registerData(long u_sec, double voltageData)
+        {
+            voltageDataList.Add(voltageData);
+            timeDataList.Add(u_sec * 10e-6);
+            formPlot.Plot.AddScatter(timeDataList.ToArray(), voltageDataList.ToArray());
+        }
     }
 }
