c01fe8651f529cb0cc121ea395a1591037220ccd
Sakai Kenta <a22ei08@hokuriku-pc.ac.jp>
2023-07-20 11:21:41 +0900

    XiaoマイコンとSerial.IO.Portとの接続確認を行った。
    コネクションの確立は行ったが、通信はまだ行っていない。

--- Test/DataReceiver.cs
+++ Test/DataReceiver.cs
@@ -3,17 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.Text;
+using System.Diagnostics;
 
 namespace Test
 {
     public class DataReceiver
     {
         private long cnt;
+
+        public bool openSerial(string comPort)
+        {
+            // シリアルポートの設定
+            SerialPort sport = new SerialPort
+            {
+                PortName = comPort,                    //ポート番号
+                BaudRate = 9600,                   //ボーレート
+                DataBits = 8,                      //データビット
+                Parity = Parity.None,              //パリティ
+                StopBits = StopBits.One,           //ストップビット
+                Handshake = Handshake.None,  //ハンドシェイク
+                Encoding = Encoding.UTF8,          //エンコード
+                WriteTimeout = 100000,             //書き込みタイムアウト
+                ReadTimeout = 100000,              //読み取りタイムアウト
+                NewLine = "\r\n"                   //改行コード指定
+            };
+            if (!sport.IsOpen)
+            {
+                try
+                {
+                    sport.Open();  // ポートオープン
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+            sport.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+            return true;
+        }
         public bool GetVoltageData(out long u_sec, out double voltage)
         {
             u_sec = cnt += 100;
             voltage = 0.0;
             return true;
         }
+
+        private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string dataReceived = sp.ReadExisting();
+
+        }
+
     }
 }
--- Test/Form1.cs
+++ Test/Form1.cs
@@ -12,10 +12,16 @@ namespace Test
 {
     public partial class Form1 : Form
     {
+        DataReceiver dataReceiver = new DataReceiver();
+        
         public Form1()
         {
             InitializeComponent();
             this.button1.Click += (sender, e) => { this.label1.Text = "Clicked"; };
+            if(dataReceiver.openSerial("COM9") == true)
+            {
+                writeLabel("Conneced!");
+            }
         }
         
         private void button1_Click(object sender, EventArgs e)
@@ -31,6 +37,11 @@ namespace Test
         private void button1_Click_1(object sender, EventArgs e)
         {
 
+        }
+
+        public void writeLabel(string str)
+        {
+            this.label1.Text = str;
         }
     }
 }
